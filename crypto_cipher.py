59c37637ffdfb780a068d14e90add8107d3801d2
lkonis <lkonis@gnresound.com>
2017-07-20 10:26:20 +0200

    new files supporting AES from Crypto.Cipher and SHA256 from Crypto.Hash

---
+++ crypto_cipher.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 13:47:30 2011
+Upgraded Mon March 2, 2015
+@author: lkonis
+"""
+
+
+import sys
+import time
+from encrypt import *
+
+class cipher:
+    mode='ENC'              # mode can be encoder or decoder
+    def run_endec(self, *args):
+      if len(args)<1:
+          args = sys.argv[1:]
+
+
+      if not args:
+        print '\ncipher.py - a python script for text file ciphering'
+        print 'the user should pick a code-word which will be used to encode a text file'
+        print 'the exact same code word will be used afterward to de-cipher that file'
+        print '\nusage: cipher infile code-word\n'
+        print 'by default, infile will be ciphered (encoded)'
+        print 'if it has a ".coded" extention, then it will be decoded'
+
+        sys.exit(1)
+
+      filename = " ".join(args[:-1])    # file to be encoded/decoded
+      password = args[-1]    # code remembered by the user
+      if len(password)<=4:
+          print ("Cipher: no password or password too short (min 4 char)\nLeaving file uncoded")
+          sys.exit(2)
+      if '.coded' in filename:
+          self.mode='DEC' # detect that file is coded and define as decoding
+      else:
+          self.mode='ENC'
+
+
+      if self.mode=='ENC':   # to encode
+          outfile = filename+".coded"
+      else:             # to decode
+          if '.coded' in filename:
+              filename = filename.split('.coded')[0]
+              outfile = filename + '.decoded'
+          else:
+              filename = filename.split('.decoded')[0]
+              outfile = filename+".decoded"
+
+      # HERE call the new PyCrypto
+      if self.mode=='ENC':
+          encrypt(getKey(password), filename)
+      else:
+          decrypt(getKey(password), filename)
+
+      if self.mode=='ENC':
+          print "Cipher: Done encoding into file: "  + outfile + "\n"
+      else:
+          print "Cipher: Done decoding\n"
+      return
+
+def main():
+    c = cipher()
+    c.run_endec()#"lkjlkj.txt", "1222")
+
+
+
+if __name__ == '__main__':
+  main()
---
+++ encrypt.py
@@ -0,0 +1,100 @@
+# encrypt.py
+# file using PyCrypto module implementing encryption and decryption methods
+
+import os, random
+
+import time
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+
+chunksize = 1024
+
+# implementing encryption
+def encrypt(key, filename):
+    outputFile = filename+".coded"
+    fileSize = str(os.path.getsize(filename)+4*16).zfill(16)
+    now = time.localtime()
+    modifyDate = "File encoded by Crypto.cipher at: %02d-%02d-%d,  %02d:%02d:%02d" % (
+        now.tm_mday, now.tm_mon, now.tm_year, now.tm_hour, now.tm_min, now.tm_sec)
+    modifyDate = modifyDate + " " * (16*4 - len(modifyDate)-1) + "\n"
+
+    # generating IV (Initializing Vector)
+    IV = ''
+    try:
+        for i in range(16):
+            IV += chr(random.randint(0, 0xFF))
+    except():
+        print "couldn't create IV (Initialize Vector). Exiting..."
+        exit(-1)
+
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
+    with open(filename, 'rb') as infile:
+            with open(outputFile, 'wb') as outfile:
+                outfile.write(fileSize)
+                outfile.write(IV)
+                outfile.write(encryptor.encrypt(modifyDate))
+
+                while True:
+                    chunk = infile.read(chunksize)
+
+                    if len(chunk) == 0:
+                        break
+                    elif len(chunk) % 16 != 0:
+                        chunk += ' '* (16 - (len(chunk)%16))
+                    outfile.write(encryptor.encrypt(chunk))
+def decrypt(key, filename):
+    outputFile = filename + ".decoded" # add postfix
+    inputFile = filename + ".coded"
+
+    with open(inputFile, 'rb') as infile:
+        filesize = long(infile.read(16))
+        IV = infile.read(16)
+        decryptor = AES.new(key, AES.MODE_CBC, IV)
+
+        with open(outputFile, 'wb') as outfile:
+            dateEnc = infile.read(16 * 4)
+            modifyDate = decryptor.decrypt(dateEnc)
+            outfile.write(modifyDate)
+
+            while True:
+                chunk = infile.read(chunksize)
+
+                if len(chunk) == 0:
+                    break
+
+                text = decryptor.decrypt(chunk)
+                outfile.write(text)
+            outfile.truncate(filesize)
+
+def getKey(password):
+    hasher = SHA256.new(password)
+    return hasher.digest()
+
+def Main():
+    choice = raw_input('Would you like to (E)ncrypt or (D)ecrypt?: ')
+    if choice == '':
+        filename = 'run_lite.bat'
+        password = '1234'
+        encrypt(getKey(password), filename)
+    elif choice=='E':
+        filename = raw_input('File to encrypt: ')
+        if filename == '':
+            filename = 'run_lite.bat'
+            password = '1234'
+        else:
+            password = raw_input('Password: ')
+        encrypt(getKey(password), filename)
+        print 'Done.'
+    elif choice == 'D':
+        filename = raw_input('File to decrypt: ')
+        if filename == '':
+            filename = 'run_lite.bat.coded'
+
+        password = raw_input('Password: ')
+        decrypt(getKey(password), filename)
+        print 'Done.'
+    else:
+        print 'No option selected'
+
+if __name__ == '__main__':
+    Main()
